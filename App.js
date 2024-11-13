b1fd225bfe68f5e1dcd5714680bf9c2b4a387e48
s-rafiq <saadrafiq2010@hotmail.co.uk>
2024-11-13 14:46:12 +0000

    installed prettier

--- App.js
+++ App.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import cities from "./data"
-import WeatherCard from "./components/WeatherCard"
-
+import React from "react";
+import cities from "./data";
+import WeatherCard from "./components/WeatherCard";
 
 function App() {
-    return (
-        <>
-            <h1 className = "title">REACTIVE WEATHER</h1>
-            <h3 className = "subtitle">Up to the minute weather news</h3>
-            <div className = "app">
-                {cities.map((listedCities, index) => <WeatherCard key={index} data={listedCities}/>)}
-            </div>
-            
-        </>
-    )
+  return (
+    <>
+      <h1 className="title">REACTIVE WEATHER</h1>
+      <h3 className="subtitle">Up to the minute weather news</h3>
+      <div className="app">
+        {cities.map((listedCities, index) => (
+          <WeatherCard key={index} data={listedCities} />
+        ))}
+      </div>
+    </>
+  );
 }
 
 export default App;
--- components/WeatherCard.js
+++ components/WeatherCard.js
@@ -1,20 +1,24 @@
 import React from "react";
-import sunny from "../assets/Sunny.svg"
-
+import sunny from "../assets/Sunny.svg";
 
 function WeatherCard(props) {
   return (
-    <div className = "card">
-        <div className = "img-container">
-            <img className="card-img-top" src = {sunny} alt="Card image cap" id = "icon"/>
-        </div>
-        <div class="card-body">
-            <h3 className="card-title">{props.data.city}</h3>
-            <h5 className="card-text">{props.data.temperature}</h5>
-            <h5 className="card-text">{props.data.forecast}</h5>
-        </div>
+    <div className="card">
+      <div className="img-container">
+        <img
+          className="card-img-top"
+          src={sunny}
+          alt="Card image cap"
+          id="icon"
+        />
+      </div>
+      <div class="card-body">
+        <h3 className="card-title">{props.data.city}</h3>
+        <h5 className="card-text">{props.data.temperature}</h5>
+        <h5 className="card-text">{props.data.forecast}</h5>
+      </div>
     </div>
   );
-};
+}
 
-module.exports = WeatherCard
+module.exports = WeatherCard;
