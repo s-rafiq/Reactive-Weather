d334a07cc3a55ada2ea48326fb864856bbdd41fb
s-rafiq <saadrafiq2010@hotmail.co.uk>
2024-11-14 16:39:21 +0000

    icons

--- components/WeatherCard.js
+++ components/WeatherCard.js
@@ -1,18 +1,32 @@
 import React from "react";
 import sunny from "../assets/Sunny.svg";
+import rainy from "../assets/Rainy.svg";
+import cloudy from "../assets/Cloudy.svg";
+import partlycloudy from "../assets/PartlyCloudy.svg";
 
 function WeatherCard(props) {
+  let icon;
+  if (props.data.forecast === "Sunny") {
+    icon = sunny;
+  } else if (props.data.forecast === "Rainy") {
+    icon = rainy;
+  } else if (props.data.forecast === "Cloudy") {
+    icon = cloudy;
+  } else if (props.data.forecast === "Partly cloudy") {
+    icon = partlycloudy;
+  }
+
   return (
     <div className="card">
       <div className="img-container">
         <img
           className="card-img-top"
-          src={sunny}
+          src={icon}
           alt="Card image cap"
           id="icon"
         />
       </div>
-      <div class="card-body">
+      <div className="card-body">
         <h3 className="card-title">{props.data.city}</h3>
         <h5 className="card-text">{props.data.temperature}</h5>
         <h5 className="card-text">{props.data.forecast}</h5>
@@ -22,3 +36,4 @@ function WeatherCard(props) {
 }
 
 module.exports = WeatherCard;
+
